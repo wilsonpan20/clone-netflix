e80efce74da2af3c1cacbe90d2aff6ae8812a03c
wilson evangelista <wilson.evangelista22@gmail.com>
2022-04-16 20:07:32 -0300

    Criando os scripts para add dados

--- ws/index.js
+++ ws/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
+const database = require('./src/services/database')
+
 const app = express();
 
 const routes = require('./src/routes/filmes.routes')
-mongoose.connect('mongodb://localhost:27017/netflix');
-
 
 
 app.use(bodyParser.json());
@@ -14,6 +13,6 @@ app.use(morgan('dev'));
 app.use('/',routes)
 
 
-app.listen(3000,()=>{
+app.listen(3001,()=>{
     console.log('Meu servidor está funcionando');
 })
